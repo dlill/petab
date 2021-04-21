9dd35c6684b4330873384469925626b78281d541
Daniel Lill <daniel.lill@posteo.de>
2021-04-21 09:40:42 +0200

    Example

---
+++ inst/petabExamples/01-EnzymeKinetics/S001-petab-export.R
@@ -0,0 +1,89 @@
+library(petab)
+try(setwd(dirname(rstudioapi::getSourceEditorContext()$path)))
+
+# -------------------------------------------------------------------------#
+# Create enzyme kinetics model and data ----
+# -------------------------------------------------------------------------#
+# .. Eqnlist and objects -----
+modelname <- "petab"
+
+el <- NULL
+el <- addReaction(el, from = "E + S", to = "ES", rate = "(kon)*E*S",
+                  description = "production of complex")
+el <- addReaction(el, from = "ES", to = "E + S", rate = "koff*ES",
+                  description = "decay of complex")
+el <- addReaction(el, from = "ES", to = "E + P", rate = "kcat*ES",
+                  description = "production of product")
+el <- eqnlist_addDefaultCompartment(el, "cytoplasm") # Need compartment information for SBML
+
+parInfo <- data.table(tibble::tribble(
+  ~parName, ~parValue, ~parUnit,
+  "kon"   ,     1  ,"litre_per_mole_per_second" ,    # Because of compartment, all dMod-fluxes are multiplied with cytoplasm volume
+  "koff"  ,     0.1,"per_second" ,
+  "kcat"  ,     0.1,"per_second" ))
+
+speciesInfo <- data.table(tibble::tribble(
+  ~speciesName, ~compName, ~initialAmount,
+  "E"         ,"cytoplasm" ,             1,          # Amount, not concentration
+  "S"         ,"cytoplasm" ,             100,
+  "ES"        ,"cytoplasm" ,             1e-12,
+  "P"         ,"cytoplasm" ,             1e-12))
+
+# compartmentInfo is left as the default getCompartmentInfo(el)
+# unitInfo is left as the default getUnitInfo(): If you need other units, you need to add them
+
+# .. Simulate Data -----
+compiled <- odemodel(f = el,modelname = modelname)
+x <- Xs(compiled, condition = "C1")
+pars <- c(setNames(parInfo$parValue, parInfo$parName),
+          setNames(speciesInfo$initialAmount, speciesInfo$speciesName))
+
+pred <- as.data.table(x(seq(0,100), pars)[[1]])
+pred <- x(seq(0,100, 10), pars)
+pred <- data.table(as.data.frame(pred))
+pred <- pred[time > 0]
+pred[,`:=`(sigma = 0.1)]
+pred <- rbind(pred,pred,pred)
+pred[,`:=`(value = exp(log(value) + rnorm(length(value), sd = sigma)))]
+pred[,`:=`(name = paste0("obs", name))]
+
+# -------------------------------------------------------------------------#
+# Export Petab ----
+# -------------------------------------------------------------------------#
+# .. Create petab tables -----
+pe_ex <- petab_experimentalCondition("C1", "C1")
+pe_ob <- petab_observables(observableId = c("obsE","obsS","obsES","obsP"),
+                           observableName = c("obsE","obsS","obsES","obsP"),
+                           observableFormula = c("E","S","ES","P"),
+                           observableTransformation = "log",
+                           noiseFormula = c("0.1"),
+                           noiseDistribution = c("normal"))
+pe_me <- petab_measurementData(observableId = pred$name,
+                               simulationConditionId = "C1",
+                               measurement = pred$value,
+                               time = pred$time,
+                               observableParameters = NA_character_,
+                               noiseParameters = pred$sigma,
+                               datasetId = "data1",
+                               replicateId = rep(1:3, each = nrow(pred)/3),
+                               preequilibrationConditionId = NA_character_)
+# .. error model -----
+pe_ob[,`:=`(noiseFormula = paste0("noiseParameter1_", observableId))]
+pe_me[,`:=`(noiseParameters = paste0("sigma_", observableId))]
+
+# ..  -----
+pe_me[observableId == "obsE",`:=`(observableParameters = "offset_E")]
+pe_mo <- petab_model(el,events = NULL,parInfo = parInfo, speciesInfo = speciesInfo)
+
+
+# .. Create petab -----
+pe <- petab(model = pe_mo,
+            experimentalCondition = pe_ex,
+            measurementData = pe_me,
+            observables = pe_ob)
+pe$parameters <- petab_create_parameter_df(pe)
+
+filename <- "petab"
+writePetab(pe, filename)
+unlink(list.files(".", "\\.o$|\\.so$|\\.c$"))
+# Exit ----
---
+++ inst/petabExamples/01-EnzymeKinetics/S002-petab-fit.R
@@ -0,0 +1,26 @@
+library(petab)
+try(setwd(dirname(rstudioapi::getSourceEditorContext()$path)))
+# -------------------------------------------------------------------------#
+# Create enzyme kinetics model and data ----
+# -------------------------------------------------------------------------#
+pd <- importPEtabSBML_indiv("petab", NFLAGcompile = 3, .compiledFolder = "Compiled")
+
+# Test model
+pred <- pd$prd(seq(0,100), pd$pars)
+pd$obj_data(pd$pars)
+
+# Test fitting
+myfit <- trust(pd$obj_data, pd$pars,1,10,iterlim = 1000)
+plotCombined(pd$prd(seq(0,100), pd$pars), pd$dModAtoms$data)
+plotCombined(pd$prd(seq(0,100), myfit$argument), pd$dModAtoms$data)
+
+# Mstrust
+center <- pepy_sample_parameter_startpoints(pd$pe, n_starts = 8)
+fits <- mstrust(pd$obj_data, center, "fit", fits = 5, iterlim = 20, cores = 8)
+fits <- as.parframe(fits)
+plotValues(fits)
+dMod_saveMstrust(fits, ".", FLAGoverwrite = TRUE)
+unlink("fit", T)
+
+
+# Exit ----
