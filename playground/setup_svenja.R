f5003a9d6b13d9345709586f49fc519dbdec3ac9
Svenja Kemmer <svenja.kemmer@fdm.uni-freiburg.de>
2021-09-20 18:13:30 +0200

    pd_iterate_through_models and petabSelect

--- playground/setup_svenja.R
+++ playground/setup_svenja.R
@@ -6,70 +6,78 @@ try(setwd(dirname(rstudioapi::getSourceEditorContext()$path)))
 # library(petab)
 devtools::load_all()
 
-# petab python package
-pepy <- petab_python_setup()
-# petab_select python package
-peps <- petab_python_setup(FLAGreturnpetabSelect = T)
+path <- "../inst/petab_select_examples/0002/selection_problem.yaml"
+iterations <- 2
 
-# -------------------------------------------------------------------------#
-# 1 select initial model  ----
-# -------------------------------------------------------------------------#
-
-selection_problem <- peps$Problem$from_yaml("../inst/petab_select_examples/0002/selection_problem.yaml")
-# test <- yaml::read_yaml("inst/petab_select_examples/0002/selection_problem.yaml")
-model_space <- selection_problem$model_space
-
-# find neighbours of the model in the model space
-# peps$model_space$ModelSpace$neighbors(candidate_space = model_space, limit = NULL, exclude = TRUE)
-
-# model_space$parameter_ids
-
-mypars <- structure(rep(0, length(model_space$parameter_ids)), names = model_space$parameter_ids)
-
-initial_virtual_model <-  peps$Model(
-  model_id='INITIAL_VIRTUAL_MODEL',
-  petab_yaml='.',
-  parameters=as.list(mypars)
-)
-
-candidate_space <- peps$ForwardCandidateSpace(initial_virtual_model)
-test_models <-  model_space$neighbors(candidate_space)
-
-# -------------------------------------------------------------------------#
-# 2 iterate through current models and generate report.yaml(s) ----
-# -------------------------------------------------------------------------#
-
-lapply(test_models, function(model){
-  id <- as.character(model$model_id)
-  print(id)
-  sink("/dev/null")
-  # create model folder
-  modelpath <- paste0("SelectionProblem/", id, "/petab")
-  dir.create(modelpath, recursive = T)
-  # generate petab
-  test_models[[1]]$to_petab(output_path = modelpath)
-  # generate pd
-      # pe<-readPetab(file.path(modelpath,"problem.yaml"))
-      # petab_plotData(pe)
-  pd <- importPEtabSBML_indiv(file.path(modelpath,"problem.yaml"), 
-                              NFLAGcompile = "0", 
-                              .compiledFolder = file.path(modelpath,"../CompiledObjects"))
-  # evaluate pd (better use ms_trust!)
-  pd_predictAndPlot2(pd)
-  # write yaml
-  pd_petabSelect_reportYaml(pd, FLAGwriteYaml = T)
-  sink()
-})
-
-# -------------------------------------------------------------------------#
-# 3 select best model ----
-# -------------------------------------------------------------------------#
+petabSelect <- function(path, iterations) {
+  # load python packages
+  pepy <- petab_python_setup()
+  peps <- petab_python_setup(FLAGreturnpetabSelect = T)
+  
+  # load selection problem and model space
+  selection_problem <- peps$Problem$from_yaml(path)
+  model_space <- selection_problem$model_space
+  
+  # generate initial model (might be replaced by python function)
+  mypars <- structure(rep(0, length(model_space$parameter_ids)), names = model_space$parameter_ids)
+  
+  initial_virtual_model <-  peps$Model(
+    model_id='INITIAL_VIRTUAL_MODEL',
+    petab_yaml='.',
+    parameters=as.list(mypars)
+  )
+  
+  # define candidate space and test criterion
+  candidate_space <- peps$ForwardCandidateSpace(initial_virtual_model)
+  mycriterion <- selection_problem$criterion
+  
+  for(i in 1:iterations){
+    # define test_models
+    test_models <- model_space$neighbors(candidate_space)
+    # iteration process
+    invisible(
+      pd_iterate_through_models(test_models)
+      )
+    
+    # define best model and select next candidate models
+    chosen_model <- selection_problem$get_best(test_models)
+    candidate_space$reset(chosen_model)
+    print(paste0("Iteration ", i, " : ", chosen_model$model_id))
+  }
+  chosen_model
+}
 
 
+pd_iterate_through_models <- function(test_models){
+  lapply(test_models, function(model){
+    # for(model in test_models){
+    id <- as.character(model$model_id)
+    print(id)
+    sink("/dev/null")
+    # create model folder
+    modelpath <- paste0("SelectionProblem/", id, "/petab")
+    dir.create(modelpath, recursive = T)
+    # generate petab
+    model$to_petab(output_path = modelpath)
+    # generate pd
+    # pe<-readPetab(file.path(modelpath,"problem.yaml"))
+    # petab_plotData(pe)
+    pd <- importPEtabSBML_indiv(file.path(modelpath,"problem.yaml"), 
+                                NFLAGcompile = "0", 
+                                .compiledFolder = file.path(modelpath,"../CompiledObjects"))
+    # evaluate pd (better use ms_trust!)
+    # pd_predictAndPlot2(pd)
+    
+    # write yaml
+    myreport <- pd_petabSelect_reportYaml(pd, FLAGwriteYaml = T)
+    
+    # include criterion in test_models object
+    criterion_value <- myreport$criteria[[mycriterion]]
+    model$set_criterion(mycriterion, criterion_value)
+    
+    sink()
+  })
+}
 
-
-
-
-# -------------------------------------------------------------------------#
-# 4 select next neighbours ----
-# -------------------------------------------------------------------------#
+mymodel <- petabSelect(path = "../inst/petab_select_examples/0002/selection_problem.yaml",
+                       iterations = 2)
