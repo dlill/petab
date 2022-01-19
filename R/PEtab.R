f03aa1e42d7d9f8e2c65bb774c90daa327f38225
Daniel Lill <daniel.lill@posteo.de>
2022-01-19 12:16:17 +0100

    safer linter when obspar is used multiple times in obsformula

--- R/PEtab.R
+++ R/PEtab.R
@@ -1212,7 +1212,7 @@ petab_lint <- function(pe) {
   if (!is.null(pe$observables) & !is.null(pe$measurementData)) {
     dco <- pe$observables[pe$measurementData, on = .NATURAL]
     dco <- unique(dco[,list(observableId, observableFormula, observableParameters)])
-    dco[,`:=`(nobspars = stringr::str_count(observableFormula,   "observableParameter"))]
+    dco[,`:=`(nobspars = do.call(c,lapply(observableFormula,   function(x) {max(as.numeric(stringr::str_extract_all(stringr::str_extract_all(x, "observableParameter\\d+", simplify = T), "\\d+", simplify = T)))})))]
     dco[,`:=`(nobsparsAvalailable = if(is.na(observableParameters)|observableParameters == "") 0 else 1+stringr::str_count(observableParameters, ";")), by = 1:nrow(dco)]
     hasMissingObsPar <- dco[,which(nobsparsAvalailable < nobspars)]
     if(length(hasMissingObsPar)) stop("The following observableIds have no observableParameters specified: ", paste0(dco[hasMissingObsPar, observableId], collapse = ", "))
@@ -1220,7 +1220,7 @@ petab_lint <- function(pe) {
     dco <- pe$observables[pe$measurementData, on = .NATURAL]
     unique(dco[,list(observableId, noiseFormula, noiseParameters)])
     dco <- unique(dco[,list(observableId, noiseFormula, noiseParameters)])
-    dco[,`:=`(nobspars = stringr::str_count(noiseFormula,   "noiseParameter"))]
+    dco[,`:=`(nobspars = do.call(c,lapply(noiseFormula,   function(x) max(as.numeric(stringr::str_extract_all(stringr::str_extract_all(x, "noiseParameter\\d+", simplify = T), "\\d+", simplify = T))))))]
     dco[,`:=`(nobsparsAvalailable = if(is.na(noiseParameters)|noiseParameters == "") 0 else 1+stringr::str_count(noiseParameters, ";")), by = 1:nrow(dco)]
     hasMissingNoisePar <- dco[,which(nobsparsAvalailable < nobspars)]
     if(length(hasMissingNoisePar)) stop("The following observableIds have no noiseParameters specified: ", paste0(dco[hasMissingNoisePar, observableId], collapse = ", "))
